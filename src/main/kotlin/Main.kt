a1164d24a16211b08df0b18e3b7319435d215848
Elena Petrova <eop79@mail.ru>
2024-02-20 11:09:21 +0300

    add attachment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -32,6 +32,80 @@ class Likes { //Информация о лайках к записи, объек
     }
 }
 
+interface Attachment {
+    val type: String
+}
+
+class ArrayAttachments() {
+    private var attachments = emptyArray<Attachment>()
+    fun addAttachment(attachment: Attachment): Attachment {
+        attachments += attachment
+        return attachments.last()
+    }
+}
+
+data class PhotoAttachment(
+    override val type: String = "photo",
+    var photo: Photo
+) : Attachment
+
+data class Photo(
+    var id: Int,
+    var owner_id: Int,
+    var link: String,
+    var anotherLink: String
+)
+
+data class ImageAttachment(
+    override val type: String = "image",
+    var image: Image
+) : Attachment
+
+data class Image(
+    var id: Int,
+    var owner_id: Int,
+    var link: String,
+    var height: Int,
+    var width: Int
+)
+
+data class VideoAttachment(
+    override val type: String = "video",
+    var video: Video
+) : Attachment
+
+data class Video(
+    var id: Int,
+    var owner_id: Int,
+    var title: String,
+    var duration: Int
+)
+
+data class AudioAttachment(
+    override val type: String = "audio",
+    var audio: Audio
+) : Attachment
+
+data class Audio(
+    var id: Int,
+    var owner_id: Int,
+    var title: String,
+    var singer: String
+)
+
+data class RepostAttachment(
+    override val type: String = "repost",
+    var repost: Repost
+) : Attachment
+
+data class Repost(
+    var id: Int,
+    var owner_id: Int,
+    var title: String,
+    var linkOwner: String
+)
+
+
 object WallService {
     private var posts = emptyArray<Post>()
     private var countId: Int = 0
@@ -40,11 +114,13 @@ object WallService {
     //добавлять запись в массив, но при этом назначать посту уникальный среди всех постов идентификатор;
     //возвращать пост с уже выставленным идентификатором.
     fun add(post: Post): Post {
-        posts += post.copy(id=++countId)
+        posts += post.copy(id = ++countId)
+
         //posts.set(countId, post)
         //countId += 1
         return posts.last()
     }
+
 
     //обновление записи
     //находить среди всех постов запись с тем же id, что и у post и обновлять все свойства;
@@ -59,16 +135,17 @@ object WallService {
     fun update(post: Post): Boolean {
         for ((index, postAny) in posts.withIndex()) {
             if (postAny.id == post.id) {
+                if (post.text != null && !post.text.equals("")) {
+                    var postAnyT: Post = post.copy(
+                        ownerId = post.ownerId, fromId = post.ownerId,
+                        text = post.text, comments = post.comments, likes = post.likes,
+                        canPin = post.canPin, canDelete = post.canDelete, canEdit = post.canEdit,
+                        isFavorite = post.isFavorite, arrayAttachments = post.arrayAttachments
+                    )
+                    posts.set(index, postAnyT)
 
-                var postAnyT: Post = post.copy(
-                    ownerId = post.ownerId, fromId = post.ownerId,
-                    text = post.text, comments = post.comments, likes = post.likes,
-                    canPin = post.canPin, canDelete = post.canDelete, canEdit = post.canEdit,
-                    isFavorite = post.isFavorite
-                )
-                posts.set(index, postAnyT)
-
-                return true
+                    return true
+                }
             }
         }
         return false
@@ -91,13 +168,14 @@ data class Post(
     var id: Int = 0, // Идентификатор записи
     var ownerId: Int, // Идентификатор владельца стены, на которой размещена запись
     var fromId: Int, // Идентификатор автора записи
-    var text: String, // Текст записи
+    var text: String?, // Текст записи
     var comments: Comments, // Информация о комментариях к записи, объект с полями
     var likes: Likes, // Информация о лайках к записи, объект с полями
     var canPin: Boolean, // Информация о том, может ли текущий пользователь закрепить запись (1 — может, 0 — не может)
     var canDelete: Boolean, // Информация о том, может ли текущий пользователь удалить запись (1 — может, 0 — не может)
     var canEdit: Boolean, // Информация о том, может ли текущий пользователь редактировать запись (1 — может, 0 — не может)
-    var isFavorite: Boolean // true, если объект добавлен в закладки у текущего пользователя
+    var isFavorite: Boolean, // true, если объект добавлен в закладки у текущего пользователя
+    var arrayAttachments: ArrayAttachments?
 )
 
 fun main() {
@@ -105,18 +183,26 @@ fun main() {
     comment1.setCount()
     val like1 = Likes()
     like1.setCount()
-    var post1 = Post(0, 1, 1, "Hello", comment1, like1, true, true, true, true)
+    var post1 = Post(0, 1, 1, "Hello", comment1, like1, true, true, true, true, null)
 
     val comment2 = Comments()
     comment2.setCount()
     val like2 = Likes()
-    var post2 = Post(0, 1, 2, "Hi", comment2, like2, true, true, true, true)
+    var post2 = Post(0, 1, 2, "Hi", comment2, like2, true, true, true, true, null)
 
     val ws = WallService
     ws.add(post1)
     ws.add(post2)
 
-    var post3 = Post(2, 3, 3, "Hi", comment2, like2, true, true, true, true)
+    val photo1 = Photo(5, 11, "https://qweqeerw", "")
+    val photoAttachment1 = PhotoAttachment("photo", photo1)
+    val photo2 = Photo(6, 11, "https://qwrw", "")
+    val photoAttachment2 = PhotoAttachment("photo", photo2)
+    var arrayAttachments1 = ArrayAttachments()
+    arrayAttachments1.addAttachment(photoAttachment1)
+    arrayAttachments1.addAttachment(photoAttachment2)
+
+    var post3 = Post(2, 3, 3, "Hi", comment2, like2, true, true, true, true, arrayAttachments1)
     ws.update(post3)
 
 
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -15,7 +15,7 @@ class WallServiceTest {
         val ws = WallService
         val comment = Comments()
         val like = Likes()
-        var post = Post(0, 1, 1, "Hello", comment, like, true, true, true, true)
+        var post = Post(0, 1, 1, "Hello", comment, like, true, true, true, true, null)
 
         val result: Boolean = ws.add(post).id != 0
         assertTrue(result)
@@ -28,10 +28,10 @@ class WallServiceTest {
         val ws = WallService
         val comment = Comments()
         val like = Likes()
-        var post = Post(0, 1, 1, "Hello", comment, like, true, true, true, true)
+        var post = Post(0, 1, 1, "Hello", comment, like, true, true, true, true, null)
         val id: Int = ws.add(post).id
 
-        var post2 = Post(id, 3, 3, "Hello", comment, like, true, true, true, true)
+        var post2 = Post(id, 3, 3, "Hello", comment, like, true, true, true, true, null)
 
         val result: Boolean = ws.update(post2)
         assertTrue(result)
@@ -43,10 +43,10 @@ class WallServiceTest {
         val ws = WallService
         val comment = Comments()
         val like = Likes()
-        var post = Post(0, 1, 1, "Hello", comment, like, true, true, true, true)
+        var post = Post(0, 1, 1, "Hello", comment, like, true, true, true, true, null)
         val id: Int = ws.add(post).id
 
-        var post2 = Post(id + 1, 3, 3, "Hello", comment, like, true, true, true, true)
+        var post2 = Post(id + 1, 3, 3, "Hello", comment, like, true, true, true, true, null)
 
         val result: Boolean = ws.update(post2)
         assertFalse(result)
